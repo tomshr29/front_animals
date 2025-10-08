f06373b57cf5c368498b2fb0da2f90328670e43b
Tomshr29 <tomscherer29@gmail.com>
2025-10-08 17:52:01 +0200

    chore: update base

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/front_animals",
+  base: "/",
   plugins: [
     tanstackRouter({
       target: "react",
